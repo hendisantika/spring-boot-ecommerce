63aab368ea7fb684a7ccab2a560a6e4d955d6ec6
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-30 20:27:16 +0700

    #5 - Add Category Model :bowtie: :frog:

---
+++ src/main/java/com/hendisantika/model/Category.java
@@ -0,0 +1,64 @@
+package com.hendisantika.model;
+
+import com.sun.istack.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Transient;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ecommerce
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/30/22
+ * Time: 20:25
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String name;
+    @Transient
+    private int productsNumber;
+    @ManyToMany(fetch = FetchType.EAGER, mappedBy = "categories")
+    private Set<Product> products;
+
+    public Category() {
+
+    }
+
+    public Category(Long id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    public int getProductsNumber() {
+        return this.products.size();
+    }
+
+}
