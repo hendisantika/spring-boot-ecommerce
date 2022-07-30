64a48551023dd450c421ffabd661f0087d2b02d3
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-30 20:32:40 +0700

    #9 - Add ShoppingCart Model :fire: :blossom:

---
+++ src/main/java/com/hendisantika/model/ShoppingCart.java
@@ -0,0 +1,54 @@
+package com.hendisantika.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Transient;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ecommerce
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/30/22
+ * Time: 20:31
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "shopping_cart")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShoppingCart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date date;
+
+    @Transient
+    private Double totalPrice;
+    @Transient
+    private int itemsNumber;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<CartItem> items = new HashSet<CartItem>();
+    private String sessionToken;
+
+}
