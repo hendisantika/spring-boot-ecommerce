0c08384f67868542e525e0c80c7a17b7cc26058d
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-30 20:35:44 +0700

    #12 - Add CarouselRepository :fire: :blossom:

---
+++ src/main/java/com/hendisantika/repository/CarouselRepository.java
@@ -0,0 +1,17 @@
+package com.hendisantika.repository;
+
+import com.hendisantika.model.Carousel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ecommerce
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/30/22
+ * Time: 20:35
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CarouselRepository extends JpaRepository<Carousel, Long> {
+}
