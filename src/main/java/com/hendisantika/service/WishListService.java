38f56b81fdc4937982b3e9266e49265c42704f8b
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-30 20:59:54 +0700

    #53 - Add addToWishFirstTime in WishListService :tiger2: :gift:

--- src/main/java/com/hendisantika/service/WishListService.java
+++ src/main/java/com/hendisantika/service/WishListService.java
@@ -1,10 +1,14 @@
 package com.hendisantika.service;
 
+import com.hendisantika.model.WishList;
+import com.hendisantika.model.WishListItem;
 import com.hendisantika.repository.WishListItemRepository;
 import com.hendisantika.repository.WishListRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +28,16 @@ public class WishListService {
     private final WishListRepository wishListRepository;
     private final WishListItemRepository wishListItemRepository;
     private final ProductService productService;
+
+    public WishList addToWishFirstTime(Long id, String sessionToken) {
+        WishList wishlist = new WishList();
+        WishListItem item = new WishListItem();
+
+        item.setDate(new Date());
+        item.setProduct(productService.getProductById(id));
+        wishlist.getItems().add(item);
+        wishlist.setSessionToken(sessionToken);
+        wishlist.setDate(new Date());
+        return wishListRepository.save(wishlist);
+    }
 }
