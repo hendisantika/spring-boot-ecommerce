ff90482d9be31cc3434e411b9f3198dccf639eb5
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-30 20:40:55 +0700

    #21 - Add addViewControllers in MvcConfig :sweat_drops: :speak_no_evil:

--- src/main/java/com/hendisantika/config/MvcConfig.java
+++ src/main/java/com/hendisantika/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.hendisantika.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -20,5 +21,10 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public MvcConfig() {
         super();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index.html");
     }
 }
