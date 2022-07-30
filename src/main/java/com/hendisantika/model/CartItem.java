f474e5761e59f3d81015ddeb2be86dc013b500b7
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-30 20:31:22 +0700

    #8 - Add CartItem Model :fire: :blossom:

---
+++ src/main/java/com/hendisantika/model/CartItem.java
@@ -0,0 +1,46 @@
+package com.hendisantika.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-ecommerce
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/30/22
+ * Time: 20:30
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "cart_item")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int quantity;
+    @Temporal(TemporalType.DATE)
+    private Date date;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id", nullable = false, updatable = false)
+    private Product product;
+}
