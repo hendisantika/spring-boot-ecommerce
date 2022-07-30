4d3f65a00c1c4881c6d39aa2c23b59a38fdd46d8
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-30 20:33:52 +0700

    #10 - Add WishListItem Model :fire: :blossom:

--- src/main/java/com/hendisantika/model/WishListItem.java
+++ src/main/java/com/hendisantika/model/WishListItem.java
@@ -4,21 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Transient;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,31 +23,21 @@ import java.util.Set;
  * Email: hendisantika@gmail.com
  * Telegram : @hendisantika34
  * Date: 7/30/22
- * Time: 20:31
+ * Time: 20:32
  * To change this template use File | Settings | File Templates.
  */
-@Entity
-@Table(name = "shopping_cart")
+@Entity(name = "wishlist_item")
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ShoppingCart {
-
+public class WishListItem {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     @Temporal(TemporalType.DATE)
     private Date date;
-
-    @Transient
-    private Double totalPrice;
-    @Transient
-    private int itemsNumber;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Set<CartItem> items = new HashSet<CartItem>();
-    private String sessionToken;
-
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id", nullable = false, updatable = false)
+    private Product product;
 }
