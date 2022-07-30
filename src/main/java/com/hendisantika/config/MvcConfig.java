52fb890c3471b5e350f1c8f8228f160b004ee357
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-30 20:41:12 +0700

    #22 - Add addResourceHandlers in MvcConfig :sweat_drops: :speak_no_evil:

--- src/main/java/com/hendisantika/config/MvcConfig.java
+++ src/main/java/com/hendisantika/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.hendisantika.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,5 +27,25 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**",
+                        "/css/**",
+                        "/resources/**",
+                        "/js/**",
+                        "/images/**",
+                        "/api/**",
+                        "/font-awesome/**"
+
+                )
+                .addResourceLocations(
+                        "classpath:/static/css/",
+                        "classpath:/static/js/",
+                        "classpath:/static/images/",
+                        "classpath:/static/api/",
+                        "classpath:/resources/",
+                        "classpath:/static/font-awesome/");
     }
 }
