ad3df6d161be5e82d17879493a67931277609552
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-30 20:56:10 +0700

    #46 - Add addShoppingCartFirstTime in ShoppingCartService :penguin: :tulip:

--- src/main/java/com/hendisantika/service/ShoppingCartService.java
+++ src/main/java/com/hendisantika/service/ShoppingCartService.java
@@ -1,10 +1,14 @@
 package com.hendisantika.service;
 
+import com.hendisantika.model.CartItem;
+import com.hendisantika.model.ShoppingCart;
 import com.hendisantika.repository.CartItemRepository;
 import com.hendisantika.repository.ShoppingCartRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +28,17 @@ public class ShoppingCartService {
     private final ShoppingCartRepository shoppingCartRepository;
     private final ProductService productService;
     private final CartItemRepository cartItemRepository;
+
+    public ShoppingCart addShoppingCartFirstTime(Long id, String sessionToken, int quantity) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        CartItem cartItem = new CartItem();
+        cartItem.setQuantity(quantity);
+        cartItem.setDate(new Date());
+        cartItem.setProduct(productService.getProductById(id));
+        shoppingCart.getItems().add(cartItem);
+        shoppingCart.setSessionToken(sessionToken);
+        shoppingCart.setDate(new Date());
+        return shoppingCartRepository.save(shoppingCart);
+
+    }
 }
