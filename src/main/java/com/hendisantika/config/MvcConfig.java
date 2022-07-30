ad3957e465a93a79f3fed05550e1c78eda3a016e
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-30 20:41:35 +0700

    #23 - Add requestContextListener in MvcConfig :sweat_drops: :speak_no_evil:

--- src/main/java/com/hendisantika/config/MvcConfig.java
+++ src/main/java/com/hendisantika/config/MvcConfig.java
@@ -1,6 +1,9 @@
 package com.hendisantika.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -47,5 +50,11 @@ public class MvcConfig implements WebMvcConfigurer {
                         "classpath:/static/api/",
                         "classpath:/resources/",
                         "classpath:/static/font-awesome/");
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(RequestContextListener.class)
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
     }
 }
