255cd9305f8143c9de2a8a9f9edd786eda96a5d2
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-31 10:12:54 +0700

    #73 - Add populateModel in GeneralController :green_book: :camera:

--- src/main/java/com/hendisantika/controller/GeneralController.java
+++ src/main/java/com/hendisantika/controller/GeneralController.java
@@ -1,11 +1,17 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.ShoppingCart;
+import com.hendisantika.model.WishList;
 import com.hendisantika.service.ProductService;
 import com.hendisantika.service.ShoppingCartService;
 import com.hendisantika.service.WishListService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +31,27 @@ public class GeneralController {
     private final ProductService productService;
     private final WishListService wishlistService;
 
+    @ModelAttribute
+    public void populateModel(Model model, HttpServletRequest request) {
+        String sessionToken = (String) request.getSession(true).getAttribute("sessionToken");
+        String sessionTokenWishList = (String) request.getSession(true).getAttribute("sessionTokenWishList");
+        if (sessionToken == null) {
+            model.addAttribute("shoppingCart", new ShoppingCart());
+
+        } else {
+            model.addAttribute("shoppingCart", shoppingCartService.getShoppingCartBySessionToken(sessionToken));
+
+        }
+
+        if (sessionTokenWishList == null) {
+            model.addAttribute("wishList", new WishList());
+
+        } else {
+            model.addAttribute("wishList", wishlistService.getWishListBySessionToken(sessionTokenWishList));
+        }
+        model.addAttribute("categories", productService.getAllCategories());
+
+        model.addAttribute("brands", productService.getAllBrands());
+        model.addAttribute("featured", productService.getProductWithBiggestDiscount());
+    }
 }
