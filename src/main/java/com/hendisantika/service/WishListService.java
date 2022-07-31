3128d741f2aabc704e6a917fc0ae4b7e8905ad8c
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-31 09:52:59 +0700

    #48 - Add getWishListBySessionToken in ShoppingCartService :penguin: :tulip:

--- src/main/java/com/hendisantika/service/WishListService.java
+++ src/main/java/com/hendisantika/service/WishListService.java
@@ -68,4 +68,8 @@ public class WishListService {
         return null;
 
     }
+
+    public WishList getWishListBySessionToken(String sessionToken) {
+        return wishListRepository.findBySessionToken(sessionToken);
+    }
 }
