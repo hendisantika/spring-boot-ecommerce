936c37ac071f32849573964484438f6ebe0cfca2
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-31 10:14:42 +0700

    #76 - Add search in IndexController :blossom: :leopard:

--- src/main/java/com/hendisantika/controller/IndexController.java
+++ src/main/java/com/hendisantika/controller/IndexController.java
@@ -1,11 +1,17 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Product;
 import com.hendisantika.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +33,15 @@ public class IndexController {
     public String showIndex(Model model) {
         model.addAttribute("products", productService.getAllProducts());
         return "index";
+    }
+
+    @GetMapping(value = "/search")
+    @ResponseBody
+    public ModelAndView search(@RequestParam("value") String value) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("fragments/searchFragment");
+        List<Product> products = productService.searchProductByNameLike(value);
+        mv.addObject("products", products);
+        return mv;
     }
 }
