814b490e6fcbd4387a4391406ff206c464aa9ec5
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-02 06:13:57 +0700

    #81 - Add static files :tiger2: :frog:

---
+++ src/main/resources/static/api/ajaxFetch.js
@@ -0,0 +1,83 @@
+/**
+ *
+ */
+function handleListProduct(event) {
+    event.preventDefault();
+    fire_ajax1();
+}
+
+function fire_ajax1() {
+    $.ajax({
+        type: "GET",
+        url: "/listProduct",
+        processData: false,// prevent jQuery from automatically
+        // transforming the data into a query string
+        contentType: false,
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+            $("#app").empty();
+            $("#app").append(data);
+        },
+        error: function (e) {
+
+            console.log("ERROR : ", e);
+
+        }
+    });
+}
+
+function handleAddProduct(event) {
+    event.preventDefault();
+    fire_ajax();
+}
+
+function fire_ajax() {
+    $.ajax({
+        type: "GET",
+        url: "/addProduct",
+        processData: false,// prevent jQuery from automatically
+        // transforming the data into a query string
+        contentType: false,
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+            $("#app").empty();
+            $("#app").append(data);
+        },
+        error: function (e) {
+
+            console.log("ERROR : ", e);
+
+        }
+    });
+}
+
+// Search 
+function dropDown(event) {
+    var val = $("#search").val();
+    if (val.length > 1) {
+        $.ajax({
+            type: "GET",
+            url: "/search?value=" + val,
+            processData: false,// prevent jQuery from automatically
+            // transforming the data into a query string
+            contentType: false,
+            cache: false,
+            timeout: 6000,
+            success: function (data) {
+                $('.dropdown-toggle').dropdown();
+                $("#dropdown-container").empty();
+                $("#dropdown-container").append(data);
+
+            },
+            error: function (e) {
+
+                console.log("ERROR : ", e);
+
+            }
+        });
+
+    }
+
+}
